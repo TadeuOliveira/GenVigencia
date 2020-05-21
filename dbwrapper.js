c550289f255b5ee57787a63e3e515f31976bda19
Tadeu Oliveira <tadeu_oliveira@id.uff.br>
2020-05-21 15:56:29 -0300

    initial commit

---
+++ dbwrapper.js
@@ -0,0 +1,50 @@
+const vigenciaHelpers = {
+    formatId: (date) => {
+        return date.toISOString().substr(0,10)
+    },
+    formatLabel: (date) => {
+        let id = date.toISOString().substr(0,10)
+        let label = id.split('-').reverse().join('/')
+        return {id,label}
+    }
+}
+
+function geraVigencia(isodate,params){
+    let { 
+        genFirstDate,
+        bloqFirstDate,
+        genNextDate,
+        bloqNextDate,
+        loopController,
+        genList,
+        forceSort
+    } = params
+
+    let primeiraVigencia = genFirstDate.func(isodate,genFirstDate.args)
+
+    let bloqPrimeiraVigencia = bloqFirstDate ? bloqFirstDate.func(primeiraVigencia,bloqFirstDate.args) : false
+
+    while(bloqPrimeiraVigencia){
+
+        primeiraVigencia = genNextDate.func(primeiraVigencia,genNextDate.args)
+
+        bloqPrimeiraVigencia = bloqFirstDate.func(primeiraVigencia,bloqFirstDate.args)
+    }
+
+    if(!genList) return primeiraVigencia
+    
+    let vigenciaList = [primeiraVigencia]
+    let proximaVigencia = primeiraVigencia
+    let loopIndex = 0
+    
+    while(loopController.func(proximaVigencia,loopIndex,loopController.args)){
+
+        proximaVigencia = genNextDate.func(primeiraVigencia,genNextDate.args)
+
+        bloqProximaVigencia = bloqNextDate.func(primeiraVigencia,bloqFirstDate.args)
+
+        if(!bloqProximaVigencia) vigenciaList.push(proximaVigencia)
+    }
+
+    return vigenciaList
+}
